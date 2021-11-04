06ca982c37767704939b6a86a09c8a5782f07db3
blizbot <93696549+blizbot@users.noreply.github.com>
2021-11-04 10:42:24 +0530

    Add files via upload

---
+++ webmanifest.php
@@ -0,0 +1,17 @@
+{
+    "name": "Bridger Land",
+    "description": "a minecraft server for pro gamers",
+    "icons": [
+        {
+            "src": "https:\/\/cdn.discordapp.com\/attachments\/839283880174288926\/839408511725076480\/c905a5e32c0fc22baf3580c0738eab1c646e9e7d.png",
+            "sizes": "192x192"
+        }
+    ],
+    "lang": "en-US",
+    "dir": "LTR",
+    "display": "minimal-ui",
+    "scope": "\/",
+    "start_url": "\/?_pwa=1",
+    "background_color": "#1c1c28",
+    "theme_color": "#28293d"
+}
